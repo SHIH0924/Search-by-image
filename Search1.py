5cd58b18b33f091f5a4377092418304452bc7849
SHIH0924 <chun09876543210@gmail.com>
2022-06-07 06:24:03 +0800

    open_file_search1

--- Search1.py
+++ Search1.py
@@ -4,33 +4,30 @@ import argparse
 import cv2
 
 import ColorDescriptor
-import Searcher
+import Searcher1
+import openfile
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--index", required=True, help="Path to where the computed index will be stored")
-
-ap.add_argument("-q", "--query", required=True, help="Path to query image")
-ap.add_argument("-r", "--result_path", required = True, help="Path to the result Path")
-args = vars(ap.parse_args())
+query=openfile.open_file()
+result_path=openfile.open_folder()
 cd = ColorDescriptor.ColorDescriptor((8,12,3))
 
 # load the query image and describe it
-query = cv2.imread(args["query"])
-features = cd.describe(query)
+filequery = cv2.imread(query)
+features = cd.describe(filequery)
 
 # perform the search
-searcher = Searcher.Searcher(args["index"])
+searcher = Searcher1.Searcher("1.csv")
 results = searcher.search(features)
 
 
 # display the query
-cv2.imshow("Query", query)
+cv2.imshow("Query", filequery)
 
 
 # loop over the results
 for(score, resultID) in results:
     # load the result image and display it
-    print(args["index"]+"/"+resultID)    
-    result = cv2.imread(args["result_path"]+"/"+resultID)  
+    print("1.csv"+"/"+resultID)    
+    result = cv2.imread(result_path+"/"+resultID)  
     cv2.imwrite(str(resultID)+'', result)
     cv2.waitKey(0)

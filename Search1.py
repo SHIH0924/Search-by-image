16a04ff91fd6a38debd50fd2348fcb5f41f21e71
SHIH0924 <chun09876543210@gmail.com>
2022-06-07 16:30:43 +0800

    add imshow

--- Search1.py
+++ Search1.py
@@ -2,7 +2,7 @@
 import argparse
 
 import cv2
-
+import numpy as np
 import ColorDescriptor
 import Searcher1
 import openfile
@@ -23,12 +23,21 @@ def Integrate_Search():
     # 顯示要查詢的圖片
     cv2.imshow("Query", filequery)
 
-
+    img=[]
     # 循環結果
     for(score, resultID) in results:
         # 加載結果圖像並儲存
         print("index.csv"+"/"+resultID)    
         result = cv2.imread(result_path+"/"+resultID)
+        cv2.imshow(str(resultID)+'',result)
+        cv2.waitKey(5000)
+        cv2.destroyWindow(str(resultID)+'')
+        cv2.waitKey(500)
         cv2.imwrite(str(resultID)+'', result)
-    cv2.waitKey(0)
+    cv2.destroyWindow("Query")
+    # 展示多個
+    #imgs = np.hstack(img)
+    #cv2.imshow("mutil_pic", imgs)
+        
+    
     
